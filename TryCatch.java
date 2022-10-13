df49a2016d6d217631af914fafcd581e98b1ccec
ysivasaipavan <siva663@sasi.ac.in>
2022-10-13 11:51:39 +0530

    first commit

---
+++ TryCatch.java
@@ -0,0 +1,15 @@
+public class TryCatch {
+    public static void main(String[] args) {
+        {
+            int a = 10, b = 0, c;
+            try {
+                c = a / b;
+                System.out.print(c);
+            } catch (Exception e) {
+                System.out.print("IT was a Math error");
+
+            }
+
+        }
+    }
+}
